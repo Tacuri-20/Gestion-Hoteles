41ea9ff70728e33d6cd855cdef136c50ca4542cb
Mateo Silva <148721903+Mzero11@users.noreply.github.com>
2024-06-04 22:18:52 -0500

    Update CheckInCheckOut.js

--- model/CheckInCheckOut.js
+++ model/CheckInCheckOut.js
@@ -1,27 +1,41 @@
 // checkincheckout.js
-const CheckInOut = {
-    fechaIngreso: null,
-    fechaSalida: null,
+
+class CheckInOut {
+    constructor() {
+        this.fechaIngreso = null;
+        this.fechaSalida = null;
+    }
 
     // Método para establecer la fecha de ingreso
-    setFechaIngreso: function(fecha) {
-        this.fechaIngreso = fecha;
-    },
+    setFechaIngreso(fecha) {
+        if (this._esFechaValida(fecha)) {
+            this.fechaIngreso = fecha;
+        } else {
+            throw new Error('Fecha de ingreso no válida');
+        }
+    }
 
     // Método para establecer la fecha de salida
-    setFechaSalida: function(fecha) {
-        this.fechaSalida = fecha;
-    },
+    setFechaSalida(fecha) {
+        if (this._esFechaValida(fecha)) {
+            this.fechaSalida = fecha;
+        } else {
+            throw new Error('Fecha de salida no válida');
+        }
+    }
 
     // Método para obtener la fecha de ingreso
-    getFechaIngreso: function() {
+    getFechaIngreso() {
         return this.fechaIngreso;
-    },
+    }
 
     // Método para obtener la fecha de salida
-    getFechaSalida: function() {
+    getFechaSalida() {
         return this.fechaSalida;
-    },
-};
+    }
+
+    
+}
 
 export default CheckInOut;
+
